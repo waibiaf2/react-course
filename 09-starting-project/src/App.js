4a041f4fdd165a12030621749a6a667ab8786b90
Waibi Andrew Franklin <waibiaf2@gmail.com>
2022-03-07 10:45:10 +0530

    used Fragment componet in react

--- 09-starting-project/src/App.js
+++ 09-starting-project/src/App.js
@@ -1,26 +1,26 @@
-import React, { useState } from 'react';
+import React, {Fragment, useState} from 'react';
 
 import AddUser from './components/Users/AddUser';
 import UsersList from './components/Users/UsersList';
 
 function App() {
-  const [usersList, setUsersList] = useState([]);
+    const [usersList, setUsersList] = useState([]);
 
-  const addUserHandler = (uName, uAge) => {
-    setUsersList((prevUsersList) => {
-      return [
-        ...prevUsersList,
-        { name: uName, age: uAge, id: Math.random().toString() },
-      ];
-    });
-  };
+    const addUserHandler = (uName, uAge) => {
+        setUsersList((prevUsersList) => {
+            return [
+                ...prevUsersList,
+                {name: uName, age: uAge, id: Math.random().toString()},
+            ];
+        });
+    };
 
-  return (
-    <div>
-      <AddUser onAddUser={addUserHandler} />
-      <UsersList users={usersList} />
-    </div>
-  );
+    return (
+        <Fragment>
+            <AddUser onAddUser={addUserHandler}/>
+            <UsersList users={usersList}/>
+        </Fragment>
+    );
 }
 
 export default App;
